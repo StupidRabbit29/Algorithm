e5d2e6f332247736cef5003de552d595988837b3
StupidRabbit29 <2922282053@qq.com>
2019-11-28 17:24:50 +0800

    update

--- 最长单调递增子序列-DP/O(n2)DP.cpp
+++ 最长单调递增子序列-DP/O(n2)DP.cpp
@@ -1,52 +1,61 @@
 #include"main.h"
 
 template<class Type>
-int binary(int i, int k, Type array[], Type dp[])
-	//用二分法找到 dp[j-1]<=array[i]<=dp[j]
-{
-	if (array[i] < dp[1])
-		return 1;
-	else
-	{
-		for (int low = 1, high = k; high != low + 1)
-		{
-			mid = (low + high) / 2;
-			if (dp[mid] > array[i])
-				high = mid;
-			else
-				low = mid;
-		}
-
-		return low;
-	}
-}
-
-template<class Type>
-int FindInc(Type array[], int num, Type dp[])
+int FindInc(Type array[], int num, Type dp[], int pre[])
 	//array存储数组
 	//num为数组的长度
-	//dp[j], 1<=j<=k，表示array[1,2,3,...,i]中所有长度为j的单调递增子序列中，结尾元素的最小值
+	//
+
 {
-	dp[1] = array[1];
-	
-	for (int i = 2, k = 1; i <= num; i++)
-		//k代表array[1,2,3,...,i]中最长单调递增子序列的长度
+	memset(dp, 0, sizeof(dp));
+	memset(pre, 0, sizeof(pre));
+
+	dp[1] = 1;
+	pre[1] = 0;
+	for (int i = 2; i <= num; i++)
 	{
-		if (array[i] > dp[k])
-			dp[++k] = array[i];
-		else
-			//用二分法找到 dp[j-1]<=array[i]<=dp[j]，令dp[j] = array[i]
-			dp[binary(i, k)] = array[i];
+		int max = 1;
+		int index = 0;
+		for (int j = 1; j < i; j++)
+		{
+			if (array[i] > array[j] && dp[j] + 1 > max)
+			{
+				max = dp[j] + 1;
+				index = j;
+			}
+		}
+
+		dp[i] = max;
+		pre[i] = index;
 	}
 
-	return k;
+	int max = 1;
+	int end = 1;
+	for (int i = 2; i <= num; i++)
+		if (dp[i] > max)
+		{
+			max = dp[i];
+			end = i;
+		}
+
+	return end;
 }
 
 //打印最长递增子序列
 template<class Type>
-void PrintInc(Type array[], int num)
+void PrintInc(Type array[], int pre[], int end)
 {
-	
+	stack<Type>my_stack;
+	for (int i = end; i > 0; i = pre[end])
+	{
+		my_stack.push(array[i]);
+	}
+
+	while (!my_stack.empty())
+	{
+		cout << my_stack.top() << endl;
+		my_stack.pop();
+	}
 }
 
-//算法复杂度：O(nlogn)
+//算法复杂度：O(n^2)
---
+++ 最长单调递增子序列-DP/O(nlogn).cpp
@@ -0,0 +1,52 @@
+#include"main.h"
+
+template<class Type>
+int binary(int i, int k, Type array[], Type dp[])
+//用二分法找到 dp[j-1]<=array[i]<=dp[j]
+{
+	if (array[i] < dp[1])
+		return 1;
+	else
+	{
+		for (int low = 1, high = k; high != low + 1)
+		{
+			mid = (low + high) / 2;
+			if (dp[mid] > array[i])
+				high = mid;
+			else
+				low = mid;
+		}
+
+		return low;
+	}
+}
+
+template<class Type>
+int FindInc(Type array[], int num, Type dp[])
+//array存储数组
+//num为数组的长度
+//dp[j], 1<=j<=k，表示array[1,2,3,...,i]中所有长度为j的单调递增子序列中，结尾元素的最小值
+{
+	dp[1] = array[1];
+
+	for (int i = 2, k = 1; i <= num; i++)
+		//k代表array[1,2,3,...,i]中最长单调递增子序列的长度
+	{
+		if (array[i] > dp[k])
+			dp[++k] = array[i];
+		else
+			//用二分法找到 dp[j-1]<=array[i]<=dp[j]，令dp[j] = array[i]
+			dp[binary(i, k)] = array[i];
+	}
+
+	return k;
+}
+
+//打印最长递增子序列
+template<class Type>
+void PrintInc(Type array[], int num)
+{
+
+}
+
+//算法复杂度：O(nlogn)
